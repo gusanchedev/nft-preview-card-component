bb4a3a66875051030c336a959a16eff545d3f0c9
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2022-01-02 16:04:21 -0500

    Add styles for mobile and desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        softblue: "hsl(215, 51%, 70%)",
+        personalizedcyan: "hsl(178, 100%, 50%)",
+        verydarkbluemainbg: "hsl(217, 54%, 11%)",
+        verydarkbluecardbg: "hsl(216, 50%, 16%)",
+        verydarkblueline: "hsl(215, 32%, 27%)",
+        personalizedwhite: "hsl(0, 0%, 100%)",
+      },
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"],
+      },
+      fontSize: {
+        project: "1.125rem",
+      },
+      backgroundImage: {
+        iconEthereum: "url('/images/image-equilibrium.jpg')",
+      },
+    },
   },
   plugins: [],
 };
